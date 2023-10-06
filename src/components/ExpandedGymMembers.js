2b0f8c24e0ccfd694a521064416db497391e6955
gaboliveiradev <g38789138@gmail.com>
2023-10-06 12:42:49 -0300

    feat: expanded componenet

--- src/components/ExpandedGymMembers.js
+++ src/components/ExpandedGymMembers.js
@@ -6,6 +6,16 @@ export default function ExpandedeGymMembers() {
     const {selectedRow} = useContext(MainContext);
 
     return (
-        <p>Apenas um teste</p>
+        <section>
+            {/* =======@ DESKTOP @======= */}
+            <article className='bg-white max-w-[862px] py-[30px] rounded-xl px-[30px] hidden lg:block'>
+                oi desktop
+            </article>
+
+             {/* =======@ MOBILE @======= */}
+             <article className='bg-white py-[30px] rounded-xlmb-[40px] px-[30px] mx-[16px] lg:hidden'>
+                oi mobile
+             </article>
+        </section>
     )
 }
