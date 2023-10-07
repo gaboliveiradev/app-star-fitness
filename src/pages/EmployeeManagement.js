0f5ebf170f9a43097a13a73a5de6e9ba8adf4206
joao <hypergamerfrr@gmail.com>
2023-10-07 01:11:45 -0300

    feat: EmployeeManagement

---
+++ src/pages/EmployeeManagement.js
@@ -0,0 +1,29 @@
+import React, { useContext, useState, } from 'react';
+import AuthContext from '../context/AuthContext';
+
+export default function EmployeeManagement() {
+
+    const { typeList, deleteType, token } = useContext(AuthContext);
+    const [records, setRecords] = useState(typeList);
+
+  return (
+    <article className="flex-auto h-full mx-auto rounded-md w-ful">
+      <div className="flex flex-col md:flex-row">
+        <div className="flex-auto pb-[14px]">
+        <h1 class="title">{`Gestão de Funcionários (${records.length})`}</h1>
+          <ul className="breadcrumbs">
+            <li>
+              <a href="#">Funcionários</a>
+            </li>
+            <li class="divider">/</li>
+            <li>
+              <a href="#" class="active">
+                Gestão de Funcionários
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </article>
+  );
+}
