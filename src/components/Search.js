b55e3a8ed6db315386ea6690c255b62cdcc1825a
joao <hypergamerfrr@gmail.com>
2023-11-04 10:12:04 -0300

    feat: Search

--- src/components/Search.js
+++ src/components/Search.js
@@ -36,12 +36,6 @@ export default function Search() {
           </div>
         )}
 
-        <button
-          type="submit"
-          class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover-bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Pesquisar
-        </button>
       </form>
     </>
   );
