46385cc186167bb03a752513adebd25f09f694b9
gaboliveiradev <g38789138@gmail.com>
2023-11-21 22:39:36 -0300

    feat: api on

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api';
+//const API_URL = 'http://127.0.0.1:8000/api';
+const API_URL = 'https://api.academiastarfitness.com.br/api';
 
 const api = axios.create({
   baseURL: API_URL,
